dd9a541cfab4ce321e4cebd8de163f795fea81d5
ibarramiguel119 <60199816+ibarramiguel119@users.noreply.github.com>
2020-05-21 10:17:11 -0500

    Add files via upload

---
+++ unidad11/PruebaLabel.java
@@ -0,0 +1,13 @@
+
+ import javax.swing.JFrame;
+
+ public class  PruebaLabel 
+ {
+ public static void main( String args[] )
+ {
+ LabelFrame marcoEtiqueta = new LabelFrame(); // crea objeto LabelFrame
+marcoEtiqueta.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+ marcoEtiqueta.setSize( 275, 180 ); // establece el tamaño del marco
+ marcoEtiqueta.setVisible( true ); // muestra el marco
+ } // fin de main
+ } // fin de la clase PruebaLabel
---
+++ unidad11/PruebaSeleccionMultiple.java
@@ -0,0 +1,15 @@
+ // Fig. 11.26: PruebaSeleccionMultiple.java
+  // Prueba de MarcoSeleccionMultiple.
+  import javax.swing.JFrame;
+ 
+  public class PruebaSeleccionMultiple
+  {
+  public static void main( String args[] )
+  {
+  MarcoSeleccionMultiple marcoSeleccionMultiple =
+  new MarcoSeleccionMultiple();
+  marcoSeleccionMultiple.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
+  marcoSeleccionMultiple.setSize( 350, 140 ); // establece el tamaño del marco
+  marcoSeleccionMultiple.setVisible( true ); // muestra el marco
+ } // fin de main
+  }
